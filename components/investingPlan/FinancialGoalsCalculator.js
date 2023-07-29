e7e4a72b79c83fd48fc4da77419b76a45c130e75
AmmarMasood <ammar.masood98@gmail.com>
2023-07-29 13:09:02 +0500

    fixed calculater

--- components/investingPlan/FinancialGoalsCalculator.js
+++ components/investingPlan/FinancialGoalsCalculator.js
@@ -1,4 +1,10 @@
-import { Button, Checkbox, InputAdornment, TextField } from "@mui/material";
+import {
+  Button,
+  Checkbox,
+  InputAdornment,
+  MenuItem,
+  TextField,
+} from "@mui/material";
 import React, { useState } from "react";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
@@ -10,6 +16,44 @@ import BarChart from "../BarChart";
 var duration = require("dayjs/plugin/duration");
 dayjs.extend(duration);
 
+const calculateForcastedAnnualReturnRate = (years) => {
+  const t =
+    Math.pow(
+      1.0 *
+        1.0 *
+        1.0 *
+        1.0 *
+        1.0 *
+        1.0 *
+        1.0 *
+        1.0 *
+        1.0 *
+        1.0227 *
+        1.0268 *
+        1.0339 *
+        1.0323 *
+        1.0285 *
+        1.0384 *
+        0.9964 *
+        1.0164 *
+        1.0316 *
+        1.0207 *
+        1.0146 *
+        1.0162 *
+        1.0012 *
+        1.0126 *
+        1.0213 *
+        1.0244 *
+        1.0181 *
+        1.0123 *
+        1.047 *
+        1.08 *
+        1.0272,
+      1 / years
+    ) - 1;
+
+  return t.toFixed(2);
+};
 const containerStyle = {
   display: "flex",
   alignItems: "center",
@@ -30,6 +74,7 @@ function FinancialGoalsCalculator({ scrollRef }) {
     monthlyLongIncome: new Date(),
     when: new Date(),
     saving: "",
+    currentGap: "",
   });
   const [showChart, setShowChart] = useState(false);
   return (
@@ -43,91 +88,31 @@ function FinancialGoalsCalculator({ scrollRef }) {
       <div style={containerStyle}>
         <p style={textStyle}>What is your investment goal ?</p>
         <TextField
+          name={`investment-goal`}
+          select
+          size="large"
           value={values.goal}
-          sx={{
-            width: "300px",
+          onChange={(e) => {
+            setValues((prv) => ({ ...prv, goal: e.target.value }));
           }}
-          type="text"
-          onChange={(e) =>
-            setValues((prv) => ({ ...prv, goal: `${e.target.value}` }))
-          }
-        />
+          style={{ width: "300px", padding: "0px" }}
+        >
+          {[
+            { key: "accumulate-wealth", value: "Accumulate wealth" },
+            { key: "regular-income", value: "Regular income" },
+            { key: "build-wealth", value: "Build wealth" },
+          ].map((option) => (
+            <MenuItem key={option.key} value={option.key}>
+              {option.value}
+            </MenuItem>
+          ))}
+        </TextField>
       </div>
-      <div style={containerStyle}>
-        <p style={textStyle}>How much will you need monthly ?</p>
-        <TextField
-          value={values.needMonthy}
-          sx={{
-            width: "300px",
-          }}
-          InputProps={{
-            startAdornment: <InputAdornment position="start">$</InputAdornment>,
-          }}
-          type="number"
-          onChange={(e) =>
-            setValues((prv) => ({ ...prv, needMonthy: `${e.target.value}` }))
-          }
-        />
-      </div>
-      <div style={containerStyle}>
-        <p style={textStyle}>How long will you need monthly income</p>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DatePicker
-            onChange={(e) =>
-              setValues((prv) => ({ ...prv, monthlyLongIncome: e.$d }))
-            }
-            // value={values.when}
-            sx={{
-              width: "300px",
-            }}
-          />
-        </LocalizationProvider>
-
-        <TextField
-          value={`${
-            dayjs.duration(dayjs(values.monthlyLongIncome).diff(new Date())).$d
-              .years
-          } year ${
-            dayjs.duration(dayjs(values.monthlyLongIncome).diff(new Date())).$d
-              .days
-          } days `}
-          disabled={true}
-          sx={{
-            width: "150px",
-            marginLeft: "20px",
-          }}
-        />
-      </div>
-      <div style={containerStyle}>
-        <p style={textStyle}>How much will you need ?</p>
-        <TextField
-          value={values.need}
-          sx={{
-            width: "300px",
-          }}
-          InputProps={{
-            startAdornment: <InputAdornment position="start">$</InputAdornment>,
-          }}
-          type="number"
-          onChange={(e) =>
-            setValues((prv) => ({ ...prv, need: `${e.target.value}` }))
-          }
-        />
-      </div>
-      <div style={containerStyle}>
-        <p style={textStyle}>Adjust for inflation?</p>
-        <Checkbox
-          checked={values.adjustedForInflation}
-          onChange={(e) =>
-            setValues((prv) => ({
-              ...prv,
-              adjustedForInflation: !prv.adjustedForInflation,
-            }))
-          }
-        />
-        {values.adjustedForInflation && (
+      {values.goal === "regular-income" && (
+        <div style={containerStyle}>
+          <p style={textStyle}>How much will you need monthly ?</p>
           <TextField
-            value={values.adjustedForInflationValue}
+            value={values.needMonthy}
             sx={{
               width: "300px",
             }}
@@ -138,74 +123,197 @@ function FinancialGoalsCalculator({ scrollRef }) {
             }}
             type="number"
             onChange={(e) =>
-              setValues((prv) => ({
-                ...prv,
-                adjustedForInflationValue: `${e.target.value}`,
-              }))
+              setValues((prv) => ({ ...prv, needMonthy: `${e.target.value}` }))
             }
           />
-        )}
-      </div>
+        </div>
+      )}
+      {values.goal === "regular-income" && (
+        <div style={containerStyle}>
+          <p style={textStyle}>How long will you need monthly income</p>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker
+              onChange={(e) =>
+                setValues((prv) => ({ ...prv, monthlyLongIncome: e.$d }))
+              }
+              // value={values.when}
+              sx={{
+                width: "300px",
+              }}
+            />
+          </LocalizationProvider>
 
-      <div style={containerStyle}>
-        <p style={textStyle}>When will you need the monthly income ?</p>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DatePicker
-            onChange={(e) => setValues((prv) => ({ ...prv, when: e.$d }))}
-            // value={values.when}
+          <TextField
+            value={`${
+              dayjs.duration(dayjs(values.monthlyLongIncome).diff(new Date()))
+                .$d.years
+            } year ${
+              dayjs.duration(dayjs(values.monthlyLongIncome).diff(new Date()))
+                .$d.days
+            } days `}
+            disabled={true}
+            sx={{
+              width: "150px",
+              marginLeft: "20px",
+            }}
+          />
+        </div>
+      )}
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <div style={{ ...containerStyle, marginRight: "20px" }}>
+          <p style={textStyle}>How much will you need ?</p>
+          <TextField
+            value={
+              values.goal === "regular-income"
+                ? 12 *
+                  values.needMonthy *
+                  dayjs.duration(
+                    dayjs(values.monthlyLongIncome).diff(new Date())
+                  ).$d.years
+                : values.need
+            }
             sx={{
               width: "300px",
             }}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">$</InputAdornment>
+              ),
+            }}
+            type="number"
+            disabled={values.goal === "regular-income"}
+            onChange={(e) =>
+              setValues((prv) => ({ ...prv, need: `${e.target.value}` }))
+            }
           />
-        </LocalizationProvider>
-
-        <TextField
-          value={`${
-            dayjs.duration(dayjs(values.when).diff(new Date())).$d.years
-          } year ${
-            dayjs.duration(dayjs(values.when).diff(new Date())).$d.days
-          } days `}
-          disabled={true}
-          sx={{
-            width: "150px",
-            marginLeft: "20px",
-          }}
-        />
+        </div>
+        <div style={containerStyle}>
+          <p style={textStyle}>Adjust for inflation?</p>
+          <Checkbox
+            checked={values.adjustedForInflation}
+            onChange={(e) =>
+              setValues((prv) => ({
+                ...prv,
+                adjustedForInflation: !prv.adjustedForInflation,
+              }))
+            }
+          />
+          {values.adjustedForInflation && (
+            <TextField
+              value={values.adjustedForInflationValue}
+              sx={{
+                width: "300px",
+              }}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">$</InputAdornment>
+                ),
+              }}
+              type="number"
+              onChange={(e) =>
+                setValues((prv) => ({
+                  ...prv,
+                  adjustedForInflationValue: `${e.target.value}`,
+                }))
+              }
+            />
+          )}
+        </div>
       </div>
-      <div style={containerStyle}>
-        <p style={textStyle}>Forcasted annualized return rate</p>
-        <TextField
-          value={values.forcastedAnnualReturnRate}
-          disabled={true}
-          type="number"
-          sx={{
-            width: "150px",
-            marginLeft: "20px",
-          }}
-          onChange={(e) =>
-            setValues((prv) => ({
-              ...prv,
-              forcastedAnnualReturnRate: `${e.target.value}`,
-            }))
-          }
-        />
-      </div>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <div style={{ ...containerStyle, marginRight: "20px" }}>
+          <p style={textStyle}>When will you need the money ?</p>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker
+              onChange={(e) => setValues((prv) => ({ ...prv, when: e.$d }))}
+              // value={values.when}
+              sx={{
+                width: "150px",
+              }}
+            />
+          </LocalizationProvider>
 
-      <div style={containerStyle}>
-        <p style={textStyle}>Have you saved anything yet ? If no enter 0</p>
-        <TextField
-          value={values.saving}
-          sx={{
-            width: "300px",
-          }}
-          InputProps={{
-            startAdornment: <InputAdornment position="start">$</InputAdornment>,
-          }}
-          type="number"
-          onChange={(e) =>
-            setValues((prv) => ({ ...prv, saving: `${e.target.value}` }))
-          }
-        />
+          <TextField
+            value={`${
+              dayjs.duration(dayjs(values.when).diff(new Date())).$d.years
+            } year ${
+              dayjs.duration(dayjs(values.when).diff(new Date())).$d.days
+            } days `}
+            disabled={true}
+            sx={{
+              width: "150px",
+              marginLeft: "20px",
+            }}
+          />
+        </div>
+        <div style={containerStyle}>
+          <p style={textStyle}>Forecasted annualized Inflation rate</p>
+          <TextField
+            value={calculateForcastedAnnualReturnRate(
+              dayjs.duration(dayjs(values.when).diff(new Date())).$d.years
+            )}
+            disabled={true}
+            type="number"
+            sx={{
+              width: "150px",
+              marginLeft: "20px",
+            }}
+            onChange={(e) =>
+              setValues((prv) => ({
+                ...prv,
+                forcastedAnnualReturnRate: `${e.target.value}`,
+              }))
+            }
+          />
+        </div>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <div style={{ ...containerStyle, marginRight: "20px" }}>
+          <p style={textStyle}>Have you saved anything yet ? If no enter 0</p>
+          <TextField
+            value={values.saving}
+            sx={{
+              width: "300px",
+            }}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">$</InputAdornment>
+              ),
+            }}
+            type="number"
+            onChange={(e) =>
+              setValues((prv) => ({ ...prv, saving: `${e.target.value}` }))
+            }
+          />
+        </div>
+        <div style={containerStyle}>
+          <p style={textStyle}>Current Gap</p>
+          <TextField
+            value={values.currentGap}
+            sx={{
+              width: "150px",
+            }}
+            type="number"
+            onChange={(e) =>
+              setValues((prv) => ({ ...prv, currentGap: `${e.target.value}` }))
+            }
+          />
+        </div>
       </div>
       <div style={{ marginTop: "30px" }}>
         <Button
